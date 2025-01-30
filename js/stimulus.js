92f555965b8665861482b6d511462bde4ef07201
Federico Iachetti <iachetti.federico@gmail.com>
2025-01-30 20:05:34 -0300

    Fix stimulus loading issue

--- js/stimulus.js
+++ js/stimulus.js
@@ -1,5 +1,3 @@
-import { Application } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
-import GameController from "./controllers/game_controller.js"
-
-const application = Application.start()
-application.register("game", GameController)
+// Iniciamos la aplicación y registramos el controlador
+const application = Stimulus.Application.start();
+application.register("game", GameController);
